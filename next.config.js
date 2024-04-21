4e45177921ffc67149bc18c4d194b7ca8143be52
dnetadmin <admin@decenternet.com>
2024-04-21 17:18:31 +0000

    Paddle Debug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    unmangedPrefetchBehavior: {
+      prefetchedResources: [
+        /cdn\.paddle\.com\/paddle\/paddle\.js/,
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
