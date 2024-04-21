eda53740a55482a4471f79a3075d16a27316b0c0
dnetadmin <admin@decenternet.com>
2024-04-21 19:34:08 +0000

    Paddle Test

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -70,7 +70,7 @@ function ProtectedPageContent() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
+          <h2 className="font-bold text-4xl mb-4">Next 1 steps</h2>
         </main>
       </div>
 
