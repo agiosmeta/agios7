018197db1271d3acd33e8eded33086cb19408e4f
dnetadmin <admin@decenternet.com>
2024-04-21 20:00:45 +0000

    Paddle Debug

--- components/paddle/PaddleProvider.tsx
+++ components/paddle/PaddleProvider.tsx
@@ -1,56 +1,34 @@
-import { useEffect, useState, createContext } from 'react';
-import Script from 'next/script';
-import axios from 'axios';
+'use client'
 
-interface PaddleContext {
-  paddle: any;
-  products: any[];
-}
+import { createContext, useState } from 'react'
+import Script from 'next/script'
 
-const PaddleContext = createContext<PaddleContext | null>(null);
+type PaddleContext = any
+
+const PaddleContext = createContext<PaddleContext>(null)
 
 const PaddleProvider = ({ children }: { children: React.ReactNode }) => {
-  const [paddle, setPaddle] = useState<any | null>(null);
-  const [products, setProducts] = useState<any[]>([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await axios.get('https://vendors.paddle.com/api/2.0/product/get_products', {
-        params: {
-          vendor_auth_code: process.env.PADDLE_VENDOR_AUTH_CODE, // Replace with your actual vendor auth code
-        },
-      });
-
-      setProducts(response.data.response.products);
-    };
-
-    fetchProducts();
-  }, []);
+  const [paddle, setPaddle] = useState(null)
 
   return (
-    <PaddleContext.Provider value={{ paddle, products }}>
+    <PaddleContext.Provider value={paddle}>
       <>
         <Script
           src="https://cdn.paddle.com/paddle/paddle.js"
           onLoad={() => {
             if (process.env.NEXT_PUBLIC_PADDLE_SANDBOX) {
-              Paddle.Environment.set('sandbox');
+              Paddle.Environment.set('sandbox')
             }
             Paddle.Setup({
-              vendor: process.env.NEXT_PUBLIC_PADDLE_VENDOR_ID,
-              eventCallback: (eventData: any) => {
-                if (eventData.eventType === 'Checkout.Loaded') {
-                  setPaddle(Paddle);
-                }
-              },
-            });
+              vendor: Number(process.env.NEXT_PUBLIC_PADDLE_VENDOR_ID),
+            })
+            setPaddle(Paddle)
           }}
-          strategy="beforeInteractive"
         />
         {children}
       </>
     </PaddleContext.Provider>
-  );
-};
+  )
+}
 
-export { PaddleProvider, PaddleContext };
+export { PaddleProvider, PaddleContext }
