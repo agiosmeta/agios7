bc967e38bc0b060129ad82d79b563521b970f187
dnetadmin <admin@decenternet.com>
2024-04-21 22:17:14 +0000

    P

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -22,7 +22,9 @@ export default async function ProtectedPage() {
 
 
  
-  return (
+  function jQuery(arg0: ($: any) => void): import("react").ReactNode {
+    throw new Error("Function not implemented.");
+  }
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
         <div className="py-6 font-bold bg-purple-950 text-center">
@@ -34,7 +36,102 @@ export default async function ProtectedPage() {
               const paddle = usePaddleContext()
 
 if (paddle) {
-  //It's loaded do some stuff
+  //Load Paddle actions
+
+
+/**
+ * Padde Button
+ *
+ * Paddle Button with Paddle.js.
+ *
+ * @since 	1.0.0
+ * @package EM
+ */
+
+jQuery( function( $ ) {
+	$( document ).ready(function() {
+		// Log.
+		console.log( "DOM Ready!" );
+		
+		$('.swalo').on('click', function(){
+			console.log( "CLICKED!" );
+			swal("Oops!", "Something went wrong on the page!", "error");
+		});
+
+ 
+
+// Function to display a sweet alert.
+function swal(title: string, message: string, type: string) {
+  swal(title, message, type);
+}
+Paddle.Setup({
+  vendor: 18989,
+  // debug: true,
+  eventCallback: function( data: any ) {
+    console.log( data );
+    console.log( 'EVENT:' + data.event)
+    if ( 'Checkout.PaymentComplete' == data.event ) {
+      console.log( '———CONVERSION!!! 🎉 ———' );
+      console.log( 'PAYMENT CHARGED:' + data.eventData.checkout.completed );
+      console.log( 'CHECKOUT ID:' + data.eventData.checkout.id );
+      console.log( 'CHECKOUT EMAIL:' + data.checkoutData.guest_email );
+      const checkout_id = data.eventData.checkout.id;
+    }
+  } // End eventCallback.
+});
+		
+		// Paddle.Order.details( checkout_id, function(order) {
+		// 				// The 'data' variable will contain order information in the format described in section one.
+		// 				console.log( order );
+		// 			});
+
+		// On click of button.
+		$( '.em_paddle_button' ).on( 'click', function() {
+			// Closed without buying.
+      function checkoutClosed(data: any): void {
+        console.log(data);
+        alert('Your purchase has been cancelled, we hope to see you again soon!');
+      }
+
+			// Bought.
+function checkoutComplete(data: any) {
+  console.log(data);
+  alert('Thanks for your purchase.');
+}
+
+// Start Checkout.
+Paddle.Checkout.open({
+  product: 'pro_01hvcx3fcwmw146qzvfy438yzx',
+  email: "test@sale.com",
+  successCallback: "checkoutComplete",
+  closeCallback: "checkoutClosed"
+}); // End checkout.
+		}); // End on click.
+	}); // End DOM Ready.
+} ) // EOF.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//End Paddle actions
 }
             </PaddleProvider>)
         
@@ -53,6 +150,19 @@ if (paddle) {
   
                             </main>
       </div>
+
+
+      <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+
+<div className="wrap">
+  <a href="#!" className="em_paddle_button">BUY NOW</a>
+  <a href="#!" className="swalo">SWAL NOW</a>
+</div>
+
+
+
+
+
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
@@ -67,5 +177,9 @@ if (paddle) {
         </p>
       </footer>
     </div>
-  );
+  ;
 }
+function swal(arg0: string, arg1: string, arg2: string) {
+  throw new Error("Function not implemented.");
+}
+
