bd130629d51596b2b2481aba30a7ad7ef1cb59f5
dnetadmin <admin@decenternet.com>
2024-04-21 23:15:12 +0000

    P

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,47 +1,11 @@
-'use client'
-
 import DeployButton from "@/components/DeployButton";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
-
-import { useEffect, useState } from 'react';
-import { initializePaddle, Paddle } from '@paddle/paddle-js';
-
-// This component could be placed under `/app/routes` or `/app/components` depending on its usage.
-export function Checkout() {
-  // Create a local state to store Paddle instance
-  const [paddle, setPaddle] = useState<Paddle | null>(null);
-
-  // Download and initialize Paddle instance from CDN
-  useEffect(() => {
-    initializePaddle({ environment: 'sandbox', token: 'test_c2d0ccf5a6158d9dee25c51ce59' }).then(
-      (paddleInstance: Paddle | undefined) => {
-        if (paddleInstance) {
-          setPaddle(paddleInstance);
-        }
-      }
-    );
-  }, []);
-
-  // Callback to open a checkout
-  const openCheckout = () => {
-    paddle?.Checkout.open({
-      items: [{ priceId: 'pri_01hvkkwfk47qzczez93g2rzxns', quantity: 1 }],
-    });
-  };
-
-  return (
-    <div>
-      <button onClick={openCheckout} disabled={!paddle}>
-        Checkout
-      </button>
-    </div>
-  );
-}
-
+import { PaddleProvider } from '@/components/paddle/PaddleProvider';
+import { usePaddleContext } from '@/components/paddle/ContextConsumer'
 
 
 export default async function ProtectedPage() {
@@ -56,13 +20,15 @@ export default async function ProtectedPage() {
   }
 
  
-  return   (
+  return (
     <div className="flex-1 w-full flex flex-col gap-10 items-center">
       <div className="w-full">
         <div className="py-6 font-bold bg-purple-950 text-center">
           This is a protected page that you can only see as an authenticated
           user
         </div>
+        
+        
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
             <DeployButton />
@@ -77,6 +43,19 @@ export default async function ProtectedPage() {
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
         </main>
       </div>
+      
+      
+      
+      (<PaddleProvider>
+  YOUR COMPONENTS
+</PaddleProvider>)
+      
+      
+      
+      
+      
+      
+      
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
@@ -92,8 +71,4 @@ export default async function ProtectedPage() {
       </footer>
     </div>
   );
-
-
-
-
 }
