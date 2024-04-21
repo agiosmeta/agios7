f49240cbea711c8cea7f08fd282e1d0eb2695cd7
dnetadmin <admin@decenternet.com>
2024-04-21 21:45:12 +0000

    P

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -19,15 +19,24 @@ export default async function ProtectedPage() {
     return redirect("/login");
   }
 
+
+
  
   return (
-    <div className="flex-1 w-full flex flex-col gap-10 items-center">
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
         <div className="py-6 font-bold bg-purple-950 text-center">
           This is a protected page that you can only see as an authenticated
           user
         </div>
         
+            (<PaddleProvider>
+              const paddle = usePaddleContext()
+
+if (paddle) {
+  //It's loaded do some stuff
+}
+            </PaddleProvider>)
         
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
@@ -41,37 +50,9 @@ export default async function ProtectedPage() {
         <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-        </main>
+  
+                            </main>
       </div>
-      
-      
-      
-      (<PaddleProvider>
-              const paddle = usePaddleContext()
-
-if (paddle) {
-  //It's loaded do some stuff
-  Paddle.Setup({
-    vendor: 18989,
-    sandbox: true,
-    environment: "sandbox",
-    display: {
-      logo: true,
-      border: "thick",
-      title: true,
-      description: true,
-      poweredBy: true,
-    },
-  })
-}
-            </PaddleProvider>)
-      
-      
-      
-      
-      
-      
-      
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
