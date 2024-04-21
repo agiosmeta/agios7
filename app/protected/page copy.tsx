4531cf2ceae7da50360057ae458f42cf67ff7f35
dnetadmin <admin@decenternet.com>
2024-04-21 15:39:33 +0000

    Paddle Debug

--- app/protected/page copy.tsx
+++ app/protected/page copy.tsx
@@ -5,18 +5,23 @@ import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 import { PaddleProvider } from '@/components/paddle/PaddleProvider';
-import { usePaddleContext } from '@/components/paddle/ContextConsumer';
+import { usePaddleContext } from '@/components/paddle/ContextConsumer'
+
 
 export default async function ProtectedPage() {
   const supabase = createClient();
-  const { data: { user } } = await supabase.auth.getUser();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   if (!user) {
     return redirect("/login");
   }
 
-  const { paddle } = usePaddleContext();
+  const paddle = usePaddleContext()
 
+ 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
@@ -36,14 +41,25 @@ export default async function ProtectedPage() {
         <Header />
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {typeof window !== 'undefined' && paddle && (
+        
+          {paddle && (
             <PaddleProvider>
-              {/* Render components that interact with Paddle here */}
-              <FetchDataSteps />
+              Render the components.
             </PaddleProvider>
           )}
+        
+        
         </main>
+      
+
+      
+      
       </div>
+
+
+ 
+
+
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
