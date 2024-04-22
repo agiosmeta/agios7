e973745ce6cb3000c886758888eb5a25d0b465cc
dnetadmin <admin@decenternet.com>
2024-04-22 00:12:16 +0000

    P

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,28 +1,33 @@
-import { GetServerSideProps } from 'next';
 import DeployButton from "@/components/DeployButton";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
 import Header from "@/components/Header";
+import { redirect } from "next/navigation";
 import { PaddleProvider } from '@/components/paddle/PaddleProvider';
 import { usePaddleContext } from '@/components/paddle/ContextConsumer'
 
-interface User {
-  id: string;
-  // other properties of the user
-}
 
-export default function ProtectedPage({ user }: { user: User }) {
+export default async function ProtectedPage() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   if (!user) {
-    return <div>Redirecting...</div>;
+    return redirect("/login");
   }
 
+ 
   return (
     <div className="flex-1 w-full flex flex-col gap-10 items-center">
       <div className="w-full">
         <div className="py-6 font-bold bg-purple-950 text-center">
-          Authenticated User
+          This is a protected page that you can only see as an authenticated
+          user
         </div>
+        
         
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
@@ -34,24 +39,27 @@ export default function ProtectedPage({ user }: { user: User }) {
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
-
         <main className="flex-1 flex flex-col gap-6">
-          <PaddleProvider>
-            <div className="flex justify-center items-center">
-              <button onClick={() => {
-                if (typeof window !== 'undefined' && window.Paddle) {
-                  window.Paddle.Checkout.open({
-                    product: 'pri_01hvkkwfk47qzczez93g2rzxns', // replace with your product id
-                  });
-                }
-              }}>
-                Open Paddle Checkout
-              </button>
-            </div>
-          </PaddleProvider>
+          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
         </main>
       </div>
+      
+      
+      
+      (<PaddleProvider>
+              const paddle = usePaddleContext()
 
+if (paddle) {
+  //It's loaded do some stuff
+}
+            </PaddleProvider>)
+      
+      
+      
+      
+      
+      
+      
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
@@ -67,22 +75,4 @@ export default function ProtectedPage({ user }: { user: User }) {
       </footer>
     </div>
   );
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const supabase = createClient();
-  const { data: { user } } = await supabase.auth.getUser();
-
-  if (!user) {
-    return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    }
-  }
-
-  return {
-    props: { user },
-  }
 }
